c888a6829dd0395bf272801b2bd93a593b0e86bc
larryhwang <larryhwang@foxmail.com>
2017-01-10 13:55:12 +0800

    Initial Commit

---
+++ VoicePlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VoicePlay
+//
+//  Created by Larry on 17/1/10.
+//  Copyright © 2017年 Larry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VoicePlay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VoicePlay
+//
+//  Created by Larry on 17/1/10.
+//  Copyright © 2017年 Larry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
